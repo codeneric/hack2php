791c16c65df22bdaa1035e2ba4c11eacd4db0808
Denis <denis@codeneric.com>
2018-06-09 19:27:30 +0200

    breaks build on travis

--- tests/HackToPhpTest.php
+++ tests/HackToPhpTest.php
@@ -14,7 +14,7 @@ namespace Facebook\HHAST;
 use function Facebook\FBExpect\expect;
 use namespace HH\Lib\{C, Str, Vec};
 
-final class HackToPHPTest extends \PHPUnit\Framework\TestCase  {
+final class HackToPHPTest extends \PHPUnit\Framework\TestCase {
 
   private function rglob(string $pattern, int $flags = 0): array<string> {
     $files = \glob($pattern, $flags);
@@ -29,27 +29,24 @@ final class HackToPHPTest extends \PHPUnit\Framework\TestCase  {
     return $files;
   }
   public function testPHPOnlyFeature(): void {
-    $d = \dirname(\dirname(__FILE__)); 
-    $t = "$d/example-files/temp";  
-    if(!\file_exists($t))
-      \mkdir($t);  
-    
+    $d = \dirname(\dirname(__FILE__));
+    $t = "$d/example-files/temp";
+    if (!\file_exists($t))
+      \mkdir($t);
+
     $files = $this->rglob("example-files/*.php");
     $i = 0;
     echo \count($files)." hack files to compile...";
-    
+
     foreach ($files as $filename) {
 
       // echo "Testing $filename...\n"; 
-      $tf = "temp_".\basename($filename); 
-      $res = \exec("$d/bin/hack2php $filename > $t/$tf && hhvm --temp-file -l $t/$tf");  
+      $tf = "temp_".\basename($filename);
+      $res = \exec("$d/bin/hack2php $filename | php -l ");
       expect($res)->toEqual(
-        null, 
+        "No syntax errors detected in -",
         "Syntax error in file $filename:\n$res",
       );
-      // if ($i >= 10){
-      //   return;}
-      //   $i++; 
     }
 
 

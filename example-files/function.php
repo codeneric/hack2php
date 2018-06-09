b1fe5bf159c4e853712327c47eaf9ae84b6ba58d
Denis <denis@codeneric.com>
2018-06-09 17:12:39 +0200

    init travis

--- example-files/function.php
+++ example-files/function.php
@@ -3,6 +3,6 @@
 namespace codeneric\phmm\legacy\validate;
 use codeneric\phmm\legacy\blub;
 
-function blub(string $v): ?blub\client_data_representation_4_0_0 {
+function blub(string $v):   ?blub\client_data_representation_4_0_0 {
     return null;
 }

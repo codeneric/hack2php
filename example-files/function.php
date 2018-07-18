a3c224bce0e3f4f8c7586f63c867046a68c8194d
Denis <denis@codeneric.com>
2018-07-18 21:43:06 +0200

    merged

--- example-files/function.php
+++ example-files/function.php
@@ -15,3 +15,13 @@ function ano(): void {
         return \in_array($a, $arr);
     };
 }
+
+
+$processAddon = function(
+    string $scripthandle,
+    string $pathFilter,
+    string $globalsVarName,
+    string $globalsFilter,
+    array<string> $dependencies,
+    string $version,
+): void {};
